d511ab20b9dc9344a979238f0e8c7903412cc11f
lithiago <thiagoramon993@gmail.com>
2026-04-15 22:22:39 -0300

    update

--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ const con = mysql.createConnection({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 
